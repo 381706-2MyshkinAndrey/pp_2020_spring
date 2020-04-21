5dde13127ceccc480de208ea1f4ba013286e2b16
amyshkin <alexander.myshkin@sarov-itc.ru>
2020-04-21 17:13:20 +0300

    Add omp release

---
+++ modules/task_2/myshkin_a_radix_sort_omp/main.cpp
@@ -0,0 +1,51 @@
+// Copyright 2020 Myshkin Andrey
+#include <numeric>
+#include <utility>
+#include <algorithm>
+#include "./ops_omp.h"
+#include <omp.h>
+#include <gtest/gtest.h>
+#include "../../../modules/task_1/myshkin_a_radix_sort_double/radix_sort_double.h"
+
+
+TEST(Senquential, Test_Numder_First) {
+    const int length = 10;
+    int sts = 0;
+    int sts2 = 0;
+    double *buffer = nullptr;
+    double *buffer2 = nullptr;
+    int num_threads = 2;
+
+    if (length > 0) {
+        buffer = reinterpret_cast<double*>(malloc(sizeof(double) * length));
+        buffer2 = reinterpret_cast<double*>(malloc(sizeof(double) * length));
+    } else {
+        sts = -1;
+        ASSERT_EQ(sts, 0);
+    }
+
+    sts2 = getRandomArray(buffer, length, -1000.0, 1000.0);
+    memcpy(buffer2, buffer, sizeof(double) * length);
+
+    sts = RadixSort(buffer, length);
+    sts2 = SortingCheck(buffer, length);
+
+    sts = RadixSortOmp(buffer2, length, num_threads);
+    sts2 = SortingCheck(buffer, length);
+
+    sts = ArrayComparison(buffer, buffer2, length);
+
+    if (sts2 == -1) sts = -1;
+
+    if (buffer) { free(buffer); buffer = nullptr; }
+    if (buffer2) { free(buffer); buffer = nullptr; }
+
+    ASSERT_EQ(sts, 0);
+}
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
---
+++ modules/task_2/myshkin_a_radix_sort_omp/radix_sort_double.h
@@ -0,0 +1,21 @@
+// Copyright 2020 Myshkin Andrey
+#ifndef MODULES_TASK_2_MYSHKIN_A_RADIX_SORT_DOUBLE_OMP_MYSHKIN_A_RADIX_SORT_DOUBLE_OMP_H_
+#define MODULES_TASK_2_MYSHKIN_A_RADIX_SORT_DOUBLE_OMP_MYSHKIN_A_RADIX_SORT_DOUBLE_OMP_H_
+#include <time.h>
+#include <stdlib.h>
+#include <random>
+#include <omp.h>
+
+int getRandomArray(double* buffer, int length, double rangebot, double rangetop);
+int ArrayComparison(double* buffer1, double *buffer2, int length);
+int MergeArraySigned(double* buffer1, int length1, double *buffer2, int length2, double *result);
+int SortingCheck(double *buffer, int length);
+
+int RadixSortUnsigned(double *buffer, int length);
+int RadixSort(double* buffer, int length);
+
+int MergeArrayForOmp(double* buffer1, int length1, double* buffer2, int length2);
+int RadixSortOmp(double* buffer, int length, int num_threads);
+
+#endif  // MODULES_TASK_2_MYSHKIN_A_RADIX_SORT_DOUBLE_OMP_MYSHKIN_A_RADIX_SORT_DOUBLE_OMP_H_
+
